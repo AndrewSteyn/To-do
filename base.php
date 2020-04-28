52828e211939c770eab039922eab343c49218d65
AndrewSteyn <andrewpvdrsteyn@gmail.com>
2020-04-28 16:22:16 +0200

    removed passwords

--- base.php
+++ base.php
@@ -2,8 +2,8 @@
 
 //login creds
 
-define ('DB_SERVER', '167.71.74.228');
-define ('DB_USERNAME', 'andrew');
-define ('DB_PASSWORD', 'Abut87Kirby');
+define ('DB_SERVER', '');
+define ('DB_USERNAME', '');
+define ('DB_PASSWORD', '');
 define ('DB_NAME', 'WDI2020_andrew');
 ?>
